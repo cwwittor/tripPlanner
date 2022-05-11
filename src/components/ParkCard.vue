4d0dc63555f303e641fdce0a770b7a0e2440f254
Swarm <swarm@Swarms-MBP-2.attlocal.net>
2022-05-11 14:26:44 -0400

    truncate cards

--- src/components/ParkCard.vue
+++ src/components/ParkCard.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="hello">
     <div class="max-w-sm flex">
-      <div class="inline-block max-w-sm rounded overflow-hidden shadow-lg">
+      <div class="inline-block max-w-sm rounded overflow-hidden shadow-lg mb-5">
         <img
           class="w-full w-96 h-64"
           :src="park.images[0].url"
           alt="Picture of the National Park at ..."
         />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{{ park.name }}</div>
-          <p class="text-gray-700 text-base">{{ park.addresses[0].city }}</p>
-        </div>
-        <div class="px-6 pt-4 pb-2">
-          <router-link
-            class="park-link"
-            :to="{ name: 'ParkDetails', params: { id: park.parkCode } }"
-          >
-            <button
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-gray-100"
+        <div class="w-96 h-30">
+          <div class="">
+            <div class="font-bold text-xl mb-2 truncate">{{ park.name }}</div>
+            <p class="text-gray-700 text-base">{{ park.addresses[0].city }}</p>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <router-link
+              class="park-link"
+              :to="{ name: 'ParkDetails', params: { id: park.parkCode } }"
             >
-              View More
-            </button>
-          </router-link>
+              <button
+                class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-gray-100"
+              >
+                View More
+              </button>
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
