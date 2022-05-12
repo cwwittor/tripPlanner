ae3e98e5f8eed8198495808c9a18f9a281f96ba2
Swarm <swarm@Swarms-MBP-2.attlocal.net>
2022-05-12 14:44:40 -0400

    Fixed Responsive Layout and Add Planner View

--- src/views/ParkList.vue
+++ src/views/ParkList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="header-bar" class="lg:w-screen lg:h-14 h-20 bg-black mb-2">
+    <div id="header-bar" class="lg:w-screen lg:h-14 h-20 bg-black">
       <span class="lg:left-0 inline-flex lg:visible invisible absolute">
         <img
           class="lg:h-14 lg:w-auto static lg:left-0"
@@ -14,15 +14,15 @@
       <button
         class="inline-block bg-gray-400 rounded-full font-semibold text-white hover:bg-gray-300 lg:mt-4 lg:right-0 lg:text-sm lg:absolute lg:mr-8 lg:px-3 lg:py-1 relative content-center text-lg mt-5 px-6 py-2"
       >
-        View Planner
+        <router-link :to="{ name: 'ParkPlanner' }"> View Planner </router-link>
       </button>
     </div>
-    <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
+    <div class="grid lg:grid-cols-5 md:grid-cols-2 grid-cols-1 gap-12 p-12">
       <ParkCard
         v-for="park in parkStore.parks.data"
         :key="park.id"
         :park="park"
-        class="justify-self-center"
+        class=""
       />
     </div>
   </div>
---
+++ src/views/ParkPlanner.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useParkStore } from "../stores";
+import { defineProps, onMounted } from "vue";
+
+const parkStore = useParkStore();
+const props = defineProps(["id"]);
+
+onMounted(() => {
+  parkStore.fetchPark(props.id).catch((error) => {
+    this.$router.push({
+      name: "ErrorDisplay",
+      params: { error: error },
+    });
+  });
+});
+</script>
+
+<template>
+  <div id="header-bar" class="py-5 bg-black">
+    <router-link
+      :to="{ name: 'ParkList' }"
+      class="rounded-full px-3 text-m font-semibold text-white hover:bg-gray-500 mt-2"
+      >Parks</router-link
+    >
+  </div>
+  <div>
+    <ul>
+      <li>First Park</li>
+    </ul>
+  </div>
+</template>
